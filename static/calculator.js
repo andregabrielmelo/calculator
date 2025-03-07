ca59c0c8953f1991d39b301f89c8d0ab31bfcc57
Redakas <andregabrielmelo@gmail.com>
2025-03-06 21:12:11 -0300

    Add function that take the expression inputed and returns the result

--- static/calculator.js
+++ static/calculator.js
@@ -11,29 +11,23 @@ const cleanExpression = (expression) => {
 
     let cleanedExpression = expression.replaceAll(" ", ""); // remove all white space
     cleanedExpression = cleanedExpression.split(""); // Get all characters of the expression in a string
-    cleanedExpression = cleanedExpression.map((item) =>
-        item in operators ? item : ` ${item} `
-    ); // Put whitespace arround operators
-    cleanedExpression = cleanedExpression.join(""); // Join array into string
-    return cleanedExpression.split(" "); // Separate the cleaned expression into numbers and operators
+    cleanedExpression = cleanedExpression.map((item) => {
+        if (operators.find((operator) => item === operator) != undefined) {
+            return ` ${item} `;
+        } else {
+            return item;
+        }
+    }); // Put whitespace arround operators
+    cleanedExpression = cleanedExpression.join("").split(" ");
+    return cleanedExpression; // Join into string and separate the cleaned expression into numbers and operators
 };
 
-const calculate = () => {
-    const expression = document.querySelector("input#expression").value; // Get math expression in input
-    let [a, operator, b] = expression.replace(" ", ""); // Get values and operator
-    const result = operate(operator, parseFloat(a), parseFloat(b)); // Calculate the result
-
-    // console.log(`A: ${a}, B: ${b}, Operator: ${operator}`);
-    console.log("Split expression: " + cleanExpression(expression));
-
-    // Show result
-    document.querySelector(
-        "p#result"
-    ).textContent = `${a} ${operator} ${b} = ${result}`;
+const calculate = (expression) => {
+    let [a, operator, b] = cleanExpression(expression); // Get values and operator
+    return operate(operator, parseFloat(a), parseFloat(b)); // Calculate the result
 };
 
 const operate = (operator, a, b) => {
-    // Store the relation between the operators and functions
     const operations = [
         {
             operator: "+",
@@ -53,14 +47,78 @@ const operate = (operator, a, b) => {
         },
     ];
 
-    // Get function related to that operator
-    const currentOperation = operations.reduce((operation, item) =>
-        item["operator"] === operator
-            ? (operation = item["function"])
-            : (operation = operation)
-    );
-    return currentOperation.function(a, b); // Get the result from the function
+    let func = operations.find((item) => item["operator"] === operator);
+    return func.function(a, b);
 };
+
+createCalculator = () => {
+    const buttons = [
+        "CE",
+        "C",
+        "DEL",
+        "/",
+        "7",
+        "8",
+        "9",
+        "x",
+        "4",
+        "5",
+        "6",
+        "-",
+        "1",
+        "2",
+        "3",
+        "+",
+        "+/-",
+        "0",
+        ",",
+        "=",
+    ];
+
+    const body = document.querySelector("body");
+
+    const container = document.createElement("div");
+    container.id = "container";
+
+    const calculator = document.createElement("div");
+    calculator.id = "calculator";
+
+    const display = document.createElement("input");
+    display.id = "display";
+    display.placeholder = "Write expression";
+    calculator.appendChild(display);
+
+    const buttonsContainer = document.createElement("div");
+    buttonsContainer.id = "buttons";
+
+    // Create and append buttons
+    buttons.map((button) => {
+        buttonElement = document.createElement("button");
+        buttonElement.id = button;
+        buttonElement.textContent = button;
+        buttonsContainer.appendChild(buttonElement);
+    });
+    calculator.appendChild(buttonsContainer);
+
+    container.appendChild(calculator);
+
+    body.appendChild(container);
+};
+
+createCalculator();
+
+const display = document.querySelector("#display");
+
+document.querySelectorAll("#buttons>button").forEach((element) =>
+    element.addEventListener("click", (event) => {
+        if (element.id != "=") display.value += event.target.id;
+    })
+);
+
+document.querySelector("[id='=']").addEventListener("click", (event) => {
+    const result = calculate(display.value);
+    display.value = result;
+});
 
 // Do not edit below this line
 exports = {
